8594524714adde21abb7f248d4e8cf065102d6f9
slicequeue <slicequeue@gmail.com>
2024-02-11 22:12:36 +0900

    feat: HelloWorld spring batch 예제 기능 구현

---
+++ src/main/java/io/spring/batch/helloworld/HelloWorldApplication.java
@@ -0,0 +1,55 @@
+package io.spring.batch.helloworld;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@EnableBatchProcessing
+@SpringBootApplication
+public class HelloWorldApplication {
+
+//	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+    public HelloWorldApplication(JobBuilderFactory jobBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+    }
+
+    @Bean
+	public Step step() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+						System.out.println("Hello World!");
+						return RepeatStatus.FINISHED; // 태스크릿이 완료됐음을 스프링 배치에게 알림
+					}
+				}).build();
+	}
+
+	@Bean
+	public Job job() {
+		return this.jobBuilderFactory.get("job")
+				.start(step())
+				.build();
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+}
